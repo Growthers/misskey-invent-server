f8c9245ecf45d6cb5f2ffe36b421a65082a01adf
Yuki Yuigishi <me@yuigishi.dev>
2022-11-20 02:12:29 +0900

    feat: express server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+import CheckEmail from "./checkEmail";
 
 const App = () => {
+   //環境変数読み込み
+   dotenv.config();
+   const serverPort = process.env.SERVER_PORT;
+   if (serverPort === undefined) {
+      console.error("Please set SERVER_PORT in .env");
+      process.exit(1);
+   }
    const app = express();
+   app.post("/api/v1/request", CheckEmail);
+   app.listen(serverPort, () => {
+      console.log("Start Misskey Invent Server");
+   });
+
 };
 
 App();
