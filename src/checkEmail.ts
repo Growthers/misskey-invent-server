adff696b67313528517772d993a169c5ae766459
Tatsuto Yamamoto <laminne33569@approvers.dev>
2022-11-20 03:39:00 +0900

    feat:メール送信の実装

--- src/checkEmail.ts
+++ src/checkEmail.ts
@@ -1,8 +1,11 @@
 import { getMisskeyInviteCode } from "./getMisskeyInviteCode";
+import {sendEmail} from "./sendEmail";
 
 const CheckEmail = async () => {
    console.log("called");
-   console.log(await getMisskeyInviteCode());
+   const code = await getMisskeyInviteCode();
+   console.log(code);
+   sendEmail("", code)
 };
 
 export default CheckEmail;
