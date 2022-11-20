0a74dcd151ef7f80429f83a11e18fa78ee454e3c
Yuki Yuigishi <me@yuigishi.dev>
2022-11-20 12:30:29 +0900

    chore: yarn format

--- src/checkEmail.ts
+++ src/checkEmail.ts
@@ -1,8 +1,7 @@
 import { Request, Response } from "express";
 import { getMisskeyInviteCode } from "./getMisskeyInviteCode";
 import { sendEmail } from "./sendEmail";
-import {checkEmailAddr} from "./checkEmailDomain";
-
+import { checkEmailAddr } from "./checkEmailDomain";
 
 type ResponseStatus = {
    status: "OK" | "NG" | "ERR";
@@ -28,22 +27,21 @@ const CheckEmail = async (req: Request, res: Response) => {
       res.send(data);
    }
 
-   if (!checkEmailAddr(email)){
+   if (!checkEmailAddr(email)) {
       const data: ResponseStatus = {
          status: "NG",
       };
       res.send(data);
    }
 
-
    const code = await getMisskeyInviteCode();
-   const r = sendEmail(email, code)
-   
+   const r = sendEmail(email, code);
+
    if (!r) {
       const data: ResponseStatus = {
-         status: "ERR"
+         status: "ERR",
       };
-      res.send(data)
+      res.send(data);
       return;
    }
 
@@ -52,7 +50,6 @@ const CheckEmail = async (req: Request, res: Response) => {
    };
    res.send(data);
    return;
-
 };
 
 export default CheckEmail;
--- src/checkEmailDomain.ts
+++ src/checkEmailDomain.ts
@@ -1,11 +1,11 @@
-import {emailDomainList} from "./domainList";
+import { emailDomainList } from "./domainList";
 // true: 高専のメールアドレス
-export function checkEmailAddr(addr: string):boolean {
-    // ドメイン部をリストと照合
-    for (let v of emailDomainList) {
-        if (addr.split("@")[1] === v) {
-            return true
-        }
-    }
-    return false;
- }
+export function checkEmailAddr(addr: string): boolean {
+   // ドメイン部をリストと照合
+   for (let v of emailDomainList) {
+      if (addr.split("@")[1] === v) {
+         return true;
+      }
+   }
+   return false;
+}
