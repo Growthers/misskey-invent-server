d79d91c5f86c5c56cd1d312d0da1a8f08b0ff3f6
Yuki Yuigishi <me@yuigishi.dev>
2022-11-23 02:54:59 +0900

    feat: nodemailer

--- src/mail/ses.ts
+++ src/mail/ses.ts
@@ -1,15 +1,65 @@
-import { IMailer, mail } from "./mailerService";
+import nodemailer, { SendMailOptions, Transporter } from "nodemailer";
+import { IMailer, Mail } from "./mailerService";
 
-export class SES implements IMailer {
-    private KEY: string;
-    private NAME: string;
+export default class SES implements IMailer {
+   private password: string;
 
-    constructor(acctKey: string, acctName: string) {
-        this.KEY = acctKey;
-        this.NAME = acctName;
-    };
+   private name: string;
 
-    async Send(body: mail): Promise<boolean> {
-        return false
-    };
+   private endpoint: string;
+
+   private port: number;
+
+   private transporter: Transporter;
+
+   constructor(
+      smtpUser: string,
+      smtpPass: string,
+      smtpEndpoint: string,
+      port: number
+   ) {
+      if (
+         smtpUser === "" ||
+         smtpPass === "" ||
+         smtpEndpoint === "" ||
+         Number.isNaN(port)
+      ) {
+         console.log("SMTP_USER", smtpUser);
+         console.log("SMTP_PASSWORD");
+         console.log("SMTP_USER", smtpUser);
+         console.log("SMTP_USER", smtpUser);
+      }
+
+      this.name = smtpUser;
+      this.password = smtpPass;
+      this.endpoint = smtpEndpoint;
+      this.port = port;
+
+      this.transporter = nodemailer.createTransport({
+         host: this.endpoint,
+         port: this.port,
+         secure: false,
+         requireTLS: true,
+         auth: {
+            user: this.name,
+            pass: this.password,
+         },
+      });
+   }
+
+   async Send(body: Mail): Promise<boolean> {
+      const mailOption: SendMailOptions = {
+         from: body.from,
+         to: body.to,
+         subject: body.subject,
+         text: body.text,
+      };
+      try {
+         await this.transporter.sendMail(mailOption);
+      } catch (e) {
+         console.error("Cannnot send Email on SES");
+         throw e;
+      }
+      return false;
+   }
 }
